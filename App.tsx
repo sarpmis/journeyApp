d3a6a87dcdc3d96be2bef730b4cb38fa651c23f8
Sarp Misoglu <sarp.misoglu@pomona.edu>
2018-07-16 13:33:37 +0300

    move globals into config

--- App.tsx
+++ App.tsx
@@ -2,29 +2,12 @@ import React from "react";
 import { Provider } from "react-redux";
 // @ts-ignore
 import store from "@redux/store";
-import { MainNavigator } from "./src/config/Router";
-// for global styling
-// @ts-ignore
-import {setCustomText} from "react-native-global-props";
+import { MainNavigator } from "@config/Router";
+import setGlobals from "@config/Globals";
 
-const globalTextProps = {
-  style: {
-    color: "#FFFFFF",
-    fontFamily: "Avenir",
-    fontSize: 12,
-    fontWeight: "900",
-    // justifyContent: "center",
-    lineHeight: 16,
-    textAlign: "center",
-  },
-};
-
-setCustomText(globalTextProps);
+setGlobals();
 
 // ENTRY POINT OF APP
-
-// export default MainNavigator;
-
 export default class App extends React.Component {
   render() {
       return (
---
+++ src/config/Globals.tsx
@@ -0,0 +1,19 @@
+// for global styling
+// @ts-ignore
+import {setCustomText} from "react-native-global-props";
+
+const globalTextProps = {
+    style: {
+      color: "#FFFFFF",
+      fontFamily: "Avenir",
+      fontSize: 12,
+      fontWeight: "900",
+      // justifyContent: "center",
+      lineHeight: 16,
+      textAlign: "center",
+    },
+  };
+
+export default function setGlobals() {
+    setCustomText(globalTextProps);
+}
